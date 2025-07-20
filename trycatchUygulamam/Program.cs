45177ca993b4c536fd2dcabe40ed91968ceda750
beyyccm <bz.cimen37@gmail.com>
2025-07-20 13:47:05 +0300

    Proje dosyası ekle.

---
+++ trycatchUygulamam/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Lütfen bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+
+            int karesi = sayi * sayi;
+            Console.WriteLine("Girdiğiniz sayının karesi: " + karesi);
+        }
+        catch
+        {
+            Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+        }
+    }
+}
